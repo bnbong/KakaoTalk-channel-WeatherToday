fc744fdfd438942b0def45a28e0866c2178018ba
bnbong <bbbong9@gmail.com>
2022-08-26 01:08:09 +0900

    [KWB-13] edit server.py

--- app/server.py
+++ app/server.py
@@ -1,2 +1,10 @@
-from app import app
+from app import app, get_db
+from .db.database import engine
 
+from .api import routing
+from .db import models, schemas, crud
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(routing.router, prefix='/api/v1')
