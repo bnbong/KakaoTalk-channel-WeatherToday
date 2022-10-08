24d267c3053a28290840c822a69a55dea0cde316
bnbong <bbbong9@gmail.com>
2022-10-08 07:59:39 +0000

    [KWB-14] add create user method

---
+++ app/db/crud.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+import os
+
+
+def get_kakao_user(db: Session, user_name: str):
+    return db.query(models.KakaoChannelUser).filter(models.KakaoChannelUser.user_name == user_name).first()
+
+def create_kakao_user(db: Session, user: schemas.KakaoUserCreate):
+    new_user = models.KakaoChannelUser(user_name=user.user_name, user_time=user.user_time, user_location_first=user.user_location_first, user_location_second=user.user_location_second, user_location_third=user.user_location_third)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
