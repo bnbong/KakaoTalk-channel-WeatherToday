c1e6cd2e0b2db670844672ad99cac3d934f07016
bnbong <bbbong9@gmail.com>
2022-10-09 07:51:56 +0000

    [KWB-1] edit model

--- app/db/models.py
+++ app/db/models.py
@@ -1,14 +1,15 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
 
 
 class KakaoChannelUser(Base):
     __tablename__ = "kakao_channel_users"
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(String(50))
-
+    id = Column(Integer, primary_key=True, index=True)
+    user_name = Column(String(50), unique=True)
     user_time = Column(String(50), default="0500")
+    is_active = Column(Boolean, default=True)
+
     user_location_first = Column(String(50))
     user_location_second = Column(String(50), nullable=True)
     user_location_third = Column(String(50), nullable=True)
