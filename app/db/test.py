68def8f7565cbb88492a76283ddc439afe8479aa
bnbong <bbbong9@gmail.com>
2022-10-11 13:57:47 +0000

    [KWB-1] add db testcase

--- app/db/test.py
+++ app/db/test.py
@@ -2,6 +2,7 @@ class TestDatabase():
     from .test_session_maker import client, TestingSessionLocal
     from . import models, schemas, crud
 
+
     @classmethod
     def setup_class(cls):
         pass
@@ -14,3 +15,8 @@ class TestDatabase():
         query = self.TestingSessionLocal().query(self.models.KakaoChannelUser).first()
 
         print(query)
+
+    def test_could_add_new_kakao_user(self):
+        response = self.client.post('/api/v1/create_kakao_user', json={"user_name":"TestUser", "user_time":"0630", "user_location_first":"TestLocation1"})
+        print(response.status_code)
+        print(response.json())
