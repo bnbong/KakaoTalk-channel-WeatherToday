c321042a818e3bb03f39005e3498319502afd47b
bnbong <bbbong9@gmail.com>
2023-01-15 04:20:34 +0000

    [KWB-3] complete testcases - could_add_new_kakao_user, could_get_kakao_user

--- app/db/test.py
+++ app/db/test.py
@@ -1,38 +1,61 @@
+# 수정 필요 : test 용도로 만들어진 test.db 에 데이터가 저장되는 것이 아닌 postgresql 컨테이너에 있는 db에 값이 저장되는 것으로 보임.
+
 class TestDatabase():
     from .test_session_maker import client, TestingSessionLocal
     from . import models, schemas, crud
 
+    from sqlalchemy.exc import InvalidRequestError, IntegrityError
+
 
     @classmethod
     def setup_class(cls):
-        pass
-
+        try:
+            db = cls.TestingSessionLocal()
+            test_user = cls.models.KakaoChannelUser(user_name="TestUser0", user_time="0800", user_location_first="TestLocation0")
+            db.add(test_user)
+            db.commit()
+            db.refresh(test_user)
+        except cls.IntegrityError:
+            print("User already added.")
+        
     @classmethod
     def teardown_class(cls):
-        pass
+        try:
+            db = cls.TestingSessionLocal()
+            target_user = db.query(cls.models.KakaoChannelUser).filter(cls.models.KakaoChannelUser.user_name == "TestUser1").first()
+            db.delete(target_user)
+            db.commit()
 
-    def test_could_fetch_database(self):
-        query = self.TestingSessionLocal().query(self.models.KakaoChannelUser).first()
-
-        print(query)
+        except cls.InvalidRequestError:
+            print("User has already deleted.")
 
     def test_could_add_new_kakao_user(self):
-        response = self.client.post('/api/v1/create_kakao_user', json={"user_name":"TestUser", "user_time":"0630", "user_location_first":"TestLocation1"})
-        
-        assert 400 == (response.status_code)
-        assert "User already exists." == (response.json().get('detail'))
+        user_data = {"user_name":"TestUser1", "user_time":"0630", "user_location_first":"TestLocation1"}
+        new_user = self.schemas.KakaoUserCreate(user_name=user_data.get("user_name"), user_time=user_data.get("user_time"), user_location_first=user_data.get("user_location_first"))
+
+        db = self.TestingSessionLocal()
+        self.crud.create_kakao_user(db=db, user=new_user)
+
+        query = db.query(self.models.KakaoChannelUser).all()
+
+        assert 2 == len(query)
+        assert (query[0].user_name) == "TestUser0"
+        assert (query[0].user_name) != "TestUser1"
+        assert (query[1].user_name) == "TestUser1"
 
     def test_could_get_kakao_user(self):
-        response = self.client.get('/api/v1/get_kakao_user', json={"user_name":"TestUser"})
-        json_response = response.json()
+        db = self.TestingSessionLocal()
+        query = db.query(self.models.KakaoChannelUser).first()
 
-        assert 200 == (response.status_code)
-        assert "TestUser" == (json_response.get('user_name'))
-        assert "TestLocation1" == (json_response.get('user_location_first'))
-        assert None == (json_response.get('user_location_second'))
-        assert None == (json_response.get('user_location_third'))
-        assert "0630" == (json_response.get('user_time'))
-        assert True == (json_response.get('is_active'))
+        assert "TestUser0" == (query.user_name)
+        assert "TestLocation0" == (query.user_location_first)
+        assert None == (query.user_location_second)
+        assert None == (query.user_location_third)
+        assert "0800" == (query.user_time)
+        assert True == (query.is_active)
+    
+    def test_should_testcase_will_not_touch_main_db(self):
+        pass
 
     def test_could_change_user_location(self):
         pass
